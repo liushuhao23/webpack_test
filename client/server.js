b8a6e2bedf5417cf29433fdc940019ce1b583525
liushuhao23 <liushuhao2@sinac.com>
2021-11-15 20:50:51 +0800

    ss

--- client/server.js
+++ client/server.js
@@ -4,18 +4,17 @@
  * @Autor: liushuhao
  * @Date: 2021-11-11 22:54:31
  * @LastEditors: liushuhao
- * @LastEditTime: 2021-11-11 23:40:08
+ * @LastEditTime: 2021-11-14 01:57:47
  */
 // "serve": "cross-env NODE_ENV=development webpack serve --config ./config/webpack.dev.conf.js",
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
-const Koa = require('koa')
 const serve = require('koa-static')
 
-const app = new Koa()
 const config = require('./config/webpack.dev.conf')
 const compiler = webpack(config)
-console.log(compiler, 'compiler');
+const express = require("express")
+const app = express()
 
 const devMiddleware = (compiler, opts) => {
     const middleware = webpackDevMiddleware(compiler, opts);
