66258f1e5dfd13084f811e1ec6d17e01b9ce2805
刘书豪 <liushuhao@cadg.cn>
2022-02-03 21:42:48 +0800

    feat: 更改rsync 配置文件

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,11 +4,11 @@
  * @Autor: liushuhao
  * @Date: 2021-10-25 17:17:33
  * @LastEditors: liushuhao
- * @LastEditTime: 2022-02-03 20:46:58
+ * @LastEditTime: 2022-02-03 21:42:43
 -->
 <template>
     <div class="index">
-        <button @click="goOther">确三生世xx定</button>
+        <button @click="goOther">确三生世 xx定</button>
         <a-table class="table" :data-source="data" :scroll="scroll" tableLayout="fixed" :pagination="false">
             <a-table-column v-for="item in title" :key="item.value" :data-index="item.value">
                 <template #title>
