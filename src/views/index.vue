2525897504f8e9ca3bd43e75dc1013afae51bc34
刘书豪 <liushuhao@cadg.cn>
2022-02-03 22:45:10 +0800

    feat: 更改rsync 为scp1

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,11 +4,11 @@
  * @Autor: liushuhao
  * @Date: 2021-10-25 17:17:33
  * @LastEditors: liushuhao
- * @LastEditTime: 2022-02-03 21:44:54
+ * @LastEditTime: 2022-02-03 22:45:07
 -->
 <template>
     <div class="index">
-        <button @click="goOther">确三生世xx定</button>
+        <button @click="goOther">确三生世XXXXxx定</button>
         <a-table class="table" :data-source="data" :scroll="scroll" tableLayout="fixed" :pagination="false">
             <a-table-column v-for="item in title" :key="item.value" :data-index="item.value">
                 <template #title>
